2df5cef7721b8fe15e1ac64594a78270fdad989a
edisonmald <edisonjhosua2007@gmail.com>
2025-05-26 12:07:51 -0500

    Add files via upload

---
+++ Bloque 3/Tarea 1.py
@@ -0,0 +1,10 @@
+
+x=5
+print('Hola')
+print('yo')
+def print_lyrics():
+    print("Soy un leñador, y estoy bien")
+    print('duermo toda la noche y trabajo todo el día')
+print_lyrics()
+x+=2
+print(x)
---
+++ Bloque 3/Tarea 5.py
@@ -0,0 +1,10 @@
+def saludo(lang):
+    if lang=='es':
+        return('hola')
+    elif lang=="fr":
+        return("Bonjour")
+    else:
+        return("hello")
+print(saludo("es"),"Glenn")
+print(saludo("fr"),"Sally")
+print(saludo("1"),"Michael")
